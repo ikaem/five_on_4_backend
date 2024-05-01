505f5cb5459c9ec402f4914398928e98a4426860
Karlo M <karlo.marinovic@decode.agency>
2024-05-01 09:18:14 +0200

    create middleware wrapper to validater register request

---
+++ bin/src/features/auth/utils/middlewares/register_with_email_and_password_request_middleware_wrapper_.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+
+import 'package:shelf/shelf.dart';
+import 'package:shelf/src/middleware.dart';
+
+import '../../../../wrappers/local/custom_middleware/custom_middleware_wrapper.dart';
+
+class RegisterWithEmailAndPasswordRequestMiddlewareWrapper
+    implements CustomMiddlewareWrapper {
+  const RegisterWithEmailAndPasswordRequestMiddlewareWrapper({
+    required FutureOr<Response?> Function(Request request)? requestHandler,
+  }) : _requestHandler = requestHandler;
+
+  final FutureOr<Response?> Function(Request request)? _requestHandler;
+
+  @override
+  Middleware call() {
+    final middleware = createMiddleware(
+      requestHandler: _requestHandler,
+    );
+    return middleware;
+  }
+}
