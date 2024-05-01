8359f66f5d05cccc03734b9e82bd5e9281234a9e
Karlo M <karlo.marinovic@decode.agency>
2024-05-01 09:26:50 +0200

    test use case to register

---
+++ bin/src/features/auth/domain/use_cases/google_login/register_with_email_and_password_use_case.dart
@@ -0,0 +1,25 @@
+import '../../repositories/auth_repository.dart';
+
+class RegisterWithEmailAndPassowordUseCase {
+  RegisterWithEmailAndPassowordUseCase({
+    required AuthRepository authRepository,
+  }) : _authRepository = authRepository;
+
+  final AuthRepository _authRepository;
+
+  Future<int?> call({
+    required String email,
+    required String password,
+    required String firstName,
+    required String lastName,
+    required String nickname,
+  }) async {
+    return await _authRepository.register(
+      email: email,
+      password: password,
+      firstName: firstName,
+      lastName: lastName,
+      nickname: nickname,
+    );
+  }
+}
---
+++ test/unit/src/features/auth/domain/use_cases/register_with_email_and_password_use_case_test.dart
@@ -0,0 +1,70 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+import '../../../../../../../bin/src/features/auth/domain/repositories/auth_repository.dart';
+import '../../../../../../../bin/src/features/auth/domain/use_cases/google_login/register_with_email_and_password_use_case.dart';
+
+void main() {
+  final authRepository = _MockAuthRepository();
+
+  // tested class
+  final registerWithEmailAndPassowordUseCase =
+      RegisterWithEmailAndPassowordUseCase(
+    authRepository: authRepository,
+  );
+
+  tearDown(() {
+    reset(authRepository);
+  });
+
+  group("$RegisterWithEmailAndPassowordUseCase", () {
+    group(".call()", () {
+      test(
+        "given valid arguments"
+        "when .call() is called"
+        "then should call AuthRepository.register and return authId",
+        () async {
+          // setup
+          when(() => authRepository.register(
+                email: any(named: "email"),
+                password: any(named: "password"),
+                firstName: any(named: "firstName"),
+                lastName: any(named: "lastName"),
+                nickname: any(named: "nickname"),
+              )).thenAnswer((i) async => 1);
+
+          // given
+          // TODO dont forget to validatze in validtor that strings are not empty
+          final email = "valid_email";
+          final password = "valid_password";
+          final firstName = "valid_firstName";
+          final lastName = "valid_lastName";
+          final nickname = "valid_nickname";
+
+          // when
+          final authId = await registerWithEmailAndPassowordUseCase.call(
+            email: email,
+            password: password,
+            firstName: firstName,
+            lastName: lastName,
+            nickname: nickname,
+          );
+
+          // then
+          verify(() => authRepository.register(
+                email: email,
+                password: password,
+                firstName: firstName,
+                lastName: lastName,
+                nickname: nickname,
+              )).called(1);
+          expect(authId, 1);
+
+          // cleanup
+        },
+      );
+    });
+  });
+}
+
+class _MockAuthRepository extends Mock implements AuthRepository {}
