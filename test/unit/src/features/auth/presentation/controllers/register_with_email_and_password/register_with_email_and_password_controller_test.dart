d311d7f91ae8eeeb3fde903f67575c09c7f5c419
Karlo M <karlo.marinovic@decode.agency>
2024-05-01 09:39:02 +0200

    add test cases for register controller

---
+++ test/unit/src/features/auth/presentation/controllers/register_with_email_and_password/register_with_email_and_password_controller_test.dart
@@ -0,0 +1,19 @@
+import 'package:test/test.dart';
+
+import '../../../../../../../../bin/src/features/auth/presentation/controllers/register_with_email_and_password/register_with_email_and_password_controller.dart';
+
+void main() {
+  group("$RegisterWithEmailAndPasswordController()", () {
+    group(".call()", () {
+      // should return expected response when user with email already exists
+
+      // should return expected response when player with created authId is not found
+
+      // should return expected response when player with created authId is found
+
+      // should return response with expected access token cookie when player with created authId is found
+
+      // should return response with expected access token in cookie when player with created authId is found
+    });
+  });
+}
