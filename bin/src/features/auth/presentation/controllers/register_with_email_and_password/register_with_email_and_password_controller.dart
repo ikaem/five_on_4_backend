f099a38e6b62e0ffa46a69bb75e8ca3ea6f6d6be
Karlo M <karlo.marinovic@decode.agency>
2024-05-02 18:53:21 +0200

    test register retruns when email exists

--- bin/src/features/auth/presentation/controllers/register_with_email_and_password/register_with_email_and_password_controller.dart
+++ bin/src/features/auth/presentation/controllers/register_with_email_and_password/register_with_email_and_password_controller.dart
@@ -1,1 +1,34 @@
-class RegisterWithEmailAndPasswordController {}
+import 'dart:io';
+
+import 'package:shelf/shelf.dart';
+
+import '../../../../core/utils/extensions/request_extension.dart';
+import '../../../../core/utils/helpers/response_generator.dart';
+import '../../../domain/use_cases/get_auth_by_email/get_auth_by_email_use_case.dart';
+import '../../../utils/constants/register_with_email_and_password_request_body_key_constants.dart';
+
+class RegisterWithEmailAndPasswordController {
+  RegisterWithEmailAndPasswordController({
+    required GetAuthByEmailUseCase getAuthByEmailUseCase,
+  }) : _getAuthByEmailUseCase = getAuthByEmailUseCase;
+
+  final GetAuthByEmailUseCase _getAuthByEmailUseCase;
+
+  Future<Response> call(
+    Request request,
+    // String id,
+  ) async {
+    final bodyMap = await request.parseBody();
+    final email =
+        bodyMap[RegisterWithEmailAndPasswordRequestBodyKeyConstants.EMAIL.value]
+            as String;
+
+    final auth = await _getAuthByEmailUseCase(email: email);
+
+    return generateResponse(
+      statusCode: HttpStatus.badRequest,
+      isOk: false,
+      message: "Invalid request - email already in use.",
+    );
+  }
+}
--- test/unit/src/features/auth/presentation/controllers/register_with_email_and_password/register_with_email_and_password_controller_test.dart
+++ test/unit/src/features/auth/presentation/controllers/register_with_email_and_password/register_with_email_and_password_controller_test.dart
@@ -1,11 +1,79 @@
+import 'dart:convert';
+
+import 'package:mocktail/mocktail.dart';
+import 'package:shelf/shelf.dart';
 import 'package:test/test.dart';
 
+import '../../../../../../../../bin/src/features/auth/domain/use_cases/get_auth_by_email/get_auth_by_email_use_case.dart';
 import '../../../../../../../../bin/src/features/auth/presentation/controllers/register_with_email_and_password/register_with_email_and_password_controller.dart';
+import '../../../../../../../../bin/src/features/auth/utils/constants/register_with_email_and_password_request_body_key_constants.dart';
+import '../../../../../../../../bin/src/features/core/domain/models/auth/auth_model.dart';
+import '../../../../../../../helpers/response.dart';
 
 void main() {
+  final getAuthByEmailUseCase = _MockGetAuthByEmailUseCase();
+  final request = _MockRequest();
+
+  // tested class
+  final registerWithEmailAndPasswordController =
+      RegisterWithEmailAndPasswordController(
+    getAuthByEmailUseCase: getAuthByEmailUseCase,
+  );
+
+  tearDown(() {
+    reset(getAuthByEmailUseCase);
+    reset(request);
+  });
+
   group("$RegisterWithEmailAndPasswordController()", () {
     group(".call()", () {
+      final requestBody = {
+        "email": "email",
+        RegisterWithEmailAndPasswordRequestBodyKeyConstants.EMAIL.value:
+            "email",
+        RegisterWithEmailAndPasswordRequestBodyKeyConstants.PASSWORD.value:
+            "password",
+        RegisterWithEmailAndPasswordRequestBodyKeyConstants.FIRST_NAME.value:
+            "firstName",
+        RegisterWithEmailAndPasswordRequestBodyKeyConstants.LAST_NAME.value:
+            "lastName",
+        RegisterWithEmailAndPasswordRequestBodyKeyConstants.NICKNAME.value:
+            "nickname",
+      };
       // should return expected response when user with email already exists
+      test(
+        "given request with email that already exists in db"
+        "when .call() is called"
+        "then should return expected response",
+        () async {
+          // setup
+
+          // given
+          when(() => request.readAsString())
+              .thenAnswer((_) async => jsonEncode(requestBody));
+          when(() => getAuthByEmailUseCase(email: any(named: "email")))
+              .thenAnswer((_) async => _testAuthModel);
+
+          // when
+          final response = await registerWithEmailAndPasswordController.call(
+            request,
+          );
+          final responseString = await response.readAsString();
+
+          // then
+          final expectedResponse = generateTestBadRequestResponse(
+              responseMessage: "Invalid request - email already in use.");
+          final expectedResponseString = await expectedResponse.readAsString();
+
+          expect(responseString, equals(expectedResponseString));
+          expect(response.statusCode, equals(expectedResponse.statusCode));
+
+          verify(() => getAuthByEmailUseCase.call(email: requestBody["email"]!))
+              .called(1);
+
+          // cleanup
+        },
+      );
 
       // should call register use case with hashed password
 
@@ -19,3 +87,15 @@ void main() {
     });
   });
 }
+
+class _MockRequest extends Mock implements Request {}
+
+class _MockGetAuthByEmailUseCase extends Mock
+    implements GetAuthByEmailUseCase {}
+
+final _testAuthModel = AuthModel(
+  id: 1,
+  email: "email",
+  createdAt: DateTime.now().millisecondsSinceEpoch,
+  updatedAt: DateTime.now().millisecondsSinceEpoch,
+);
