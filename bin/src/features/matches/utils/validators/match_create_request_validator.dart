5f8e410e03571e46c983f3bece38891f2c10a81e
Karlo M <karlo.marinovic@decode.agency>
2024-05-03 16:25:04 +0200

    refactor response return from match create request validator

--- bin/src/features/matches/utils/validators/match_create_request_validator.dart
+++ bin/src/features/matches/utils/validators/match_create_request_validator.dart
@@ -1,9 +1,12 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:shelf/shelf.dart';
 
+import '../../../core/domain/values/response_body_value.dart';
 import '../../../core/utils/extensions/request_extension.dart';
+import '../../../core/utils/helpers/response_generator.dart';
 import '../constants/match_create_request_body_key_constants.dart';
 
 class MatchCreateRequestValidator {
@@ -14,40 +17,90 @@ class MatchCreateRequestValidator {
 
     final title = body[MatchCreateRequestBodyKeyConstants.TITLE.value];
     if (title is! String) {
-      return _generateBadRequestResponse(
-        responseMessage: "Title is required.",
+      final responseBody = ResponseBodyValue(
+        message: "Title is required.",
+        ok: false,
       );
+      return generateResponse(
+        statusCode: HttpStatus.badRequest,
+        body: responseBody,
+        // TODD come back to this - maybe it is not needed on validation for cookies to be updated
+        // TODO but then frontend will need to check if cookies exist
+        cookies: [],
+      );
+
+      // return _generateBadRequestResponse(
+      //   responseMessage: "Title is required.",
+      // );
     }
 
     final description =
         body[MatchCreateRequestBodyKeyConstants.DESCRIPTION.value];
     if (description is! String) {
-      return _generateBadRequestResponse(
-        responseMessage: "Description is required.",
+      final responseBody = ResponseBodyValue(
+        message: "Description is required.",
+        ok: false,
       );
+      return generateResponse(
+        statusCode: HttpStatus.badRequest,
+        body: responseBody,
+        cookies: [],
+      );
+
+      // return _generateBadRequestResponse(
+      //   responseMessage: "Description is required.",
+      // );
     }
 
     final dateAndTime =
         body[MatchCreateRequestBodyKeyConstants.DATE_AND_TIME.value];
     if (dateAndTime is! int) {
-      return _generateBadRequestResponse(
-        responseMessage: "Date and time is required.",
+      final responseBody = ResponseBodyValue(
+        message: "Date and time is required.",
+        ok: false,
       );
+      return generateResponse(
+        statusCode: HttpStatus.badRequest,
+        body: responseBody,
+        cookies: [],
+      );
+      // return _generateBadRequestResponse(
+      //   responseMessage: "Date and time is required.",
+      // );
     }
 
     // TODO maybe validate if dateAndTime is in the future
     final matchDate = DateTime.fromMillisecondsSinceEpoch(dateAndTime);
     if (matchDate.isBefore(DateTime.now())) {
-      return _generateBadRequestResponse(
-        responseMessage: "Date and time must be in the future.",
+      final responseBody = ResponseBodyValue(
+        message: "Date and time must be in the future.",
+        ok: false,
       );
+      return generateResponse(
+        statusCode: HttpStatus.badRequest,
+        body: responseBody,
+        cookies: [],
+      );
+
+      // return _generateBadRequestResponse(
+      //   responseMessage: "Date and time must be in the future.",
+      // );
     }
 
     final location = body[MatchCreateRequestBodyKeyConstants.LOCATION.value];
     if (location is! String) {
-      return _generateBadRequestResponse(
-        responseMessage: "Location is required.",
+      final responseBody = ResponseBodyValue(
+        message: "Location is required.",
+        ok: false,
       );
+      return generateResponse(
+        statusCode: HttpStatus.badRequest,
+        body: responseBody,
+        cookies: [],
+      );
+      // return _generateBadRequestResponse(
+      //   responseMessage: "Location is required.",
+      // );
     }
 
     return null;
@@ -55,20 +108,20 @@ class MatchCreateRequestValidator {
 }
 
 // TODO maybe is possible to abstract this and make it generic and reuslable by all validators - or to live in some base middleware class
-Response _generateBadRequestResponse({
-  required String responseMessage,
-}) {
-  final response = Response.badRequest(
-    body: jsonEncode(
-      {
-        "ok": false,
-        "message": responseMessage,
-      },
-    ),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  );
+// Response _generateBadRequestResponse({
+//   required String responseMessage,
+// }) {
+//   final response = Response.badRequest(
+//     body: jsonEncode(
+//       {
+//         "ok": false,
+//         "message": responseMessage,
+//       },
+//     ),
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//   );
 
-  return response;
-}
+//   return response;
+// }
