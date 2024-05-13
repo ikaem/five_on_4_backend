a41999b81faefb223b36fbbcf70e122ce2054648
Karlo M <karlo.marinovic@decode.agency>
2024-05-13 12:09:13 +0200

    test failure generator

--- test/unit/src/features/core/utils/helpers/response_generator_test.dart
+++ test/unit/src/features/core/utils/helpers/response_generator_test.dart
@@ -8,10 +8,98 @@ import '../../../../../../../bin/src/features/core/utils/helpers/response_genera
 
 void main() {
   group("$ResponseGenerator", () {
+    group("failure", () {
+      // should return expected response
+      test(
+        "given required arguments"
+        "when .failure() is called"
+        "then should return expected response",
+        () async {
+          // setup
+
+          // given
+          final message = "message";
+          final statusCode = HttpStatus.badRequest;
+
+          // when
+          final response = ResponseGenerator.failure(
+            message: message,
+            statusCode: statusCode,
+          );
+          final responseBodyString = await response.readAsString();
+
+          // then
+          final expectedResponse = Response(statusCode,
+              body: jsonEncode({
+                "ok": false,
+                "message": message,
+              }));
+          final expectedResponseBodyString =
+              await expectedResponse.readAsString();
+
+          expect(response.statusCode, expectedResponse.statusCode);
+          expect(responseBodyString, expectedResponseBodyString);
+
+          // cleanup
+        },
+      );
+
+      // should return response with expected status code
+      test(
+        "given status code is provided"
+        "when .failure() is called"
+        "then should return response with expected status code",
+        () async {
+          // setup
+
+          // given
+          final message = "message";
+          final statusCode = HttpStatus.internalServerError;
+
+          // when
+          final response = ResponseGenerator.failure(
+            message: message,
+            statusCode: statusCode,
+          );
+
+          // then
+          expect(response.statusCode, statusCode);
+
+          // cleanup
+        },
+      );
+
+      // should return response with json content type
+      test(
+        "given response is returned "
+        "when examine response headers "
+        "then should have content type set as json",
+        () async {
+          // setup
+          final message = "message";
+          final statusCode = HttpStatus.badRequest;
+
+          // given
+          final response = ResponseGenerator.failure(
+            message: message,
+            statusCode: statusCode,
+          );
+
+          // when/then
+          expect(
+            response.headers[HttpHeaders.contentTypeHeader],
+            "application/json",
+          );
+
+          // cleanup
+        },
+      );
+    });
+
     group(".success()", () {
       test(
         "given required arguments "
-        "when .generate() is called"
+        "when .success() is called"
         "then should return expected response",
         () async {
           // setup
@@ -47,7 +135,7 @@ void main() {
       // given custom http status code is provided, then should return expected response
       test(
         "given custom http status code is provided "
-        "when .generate() is called"
+        "when .success() is called"
         "then should return expected response",
         () async {
           // setup
@@ -63,20 +151,10 @@ void main() {
             data: data,
             statusCode: statusCode,
           );
-          final responseBodyString = await response.readAsString();
 
           // then
-          final expectedResponse = Response(statusCode,
-              body: jsonEncode({
-                "ok": true,
-                "message": message,
-                "data": data,
-              }));
-          final expectedResponseBodyString =
-              await expectedResponse.readAsString();
 
-          expect(response.statusCode, expectedResponse.statusCode);
-          expect(responseBodyString, expectedResponseBodyString);
+          expect(response.statusCode, statusCode);
 
           // cleanup
         },
@@ -85,7 +163,7 @@ void main() {
       // given no data is provided, then should return expected response
       test(
         "given no data is provided "
-        "when .generate() is called "
+        "when .success() is called "
         "then should return expected response",
         () async {
           // setup
@@ -124,18 +202,16 @@ void main() {
         "then should have content type set as json",
         () async {
           // setup
-
-          // given
           final message = "message";
           final data = {"data": "data"};
 
-          // when
+          // given
           final response = ResponseGenerator.success(
             message: message,
             data: data,
           );
 
-          // then
+          // when / then
           expect(
             response.headers[HttpHeaders.contentTypeHeader],
             "application/json",
