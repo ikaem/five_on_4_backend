f7925417038f1e352da7948c4629ab494a79ceda
Karlo M <karlo.marinovic@decode.agency>
2024-05-25 10:51:40 +0200

    chore: add makefile to gitignore

--- bin/main.dart
+++ bin/main.dart
@@ -5,8 +5,11 @@ import 'bootstrap.dart';
 void main(List<String> args) {
   // final envVarTest1 = const String.fromEnvironment('ENV_VAR_TEST1');
   // final envVarTest2 = const String.fromEnvironment('ENV_VAR_TEST2');
-  final envVarTest1 = Platform.environment['ENV_VAR_TEST1'];
+  final envVarTest1 = Platform.environment['PGHOST'];
   final envVarTest2 = Platform.environment['ENV_VAR_TEST2'];
+
+  print("ENV_VAR_TEST1: $envVarTest1");
+  print("ENV_VAR_TEST2: $envVarTest2");
 
   boostrap();
 }
