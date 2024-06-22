b0532c8de8e095ba556d83ecc2f9c09e6d882d6a
Karlo M <karlo.marinovic@decode.agency>
2024-06-22 07:12:39 +0200

    test get player matches middleware

---
+++ test/unit/src/features/matches/utils/middlewares/get_player_matches_overview_request_middleware_wrapper_test.dart
@@ -0,0 +1,62 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:shelf/shelf.dart';
+import 'package:test/test.dart';
+
+import '../../../../../../../bin/src/features/matches/utils/middlewares/get_player_matches_overview_request_middleware_wrapper.dart';
+import '../../../../../../../bin/src/features/matches/utils/validators/get_player_matches_overview_request_validator.dart';
+
+void main() {
+  group("$GetPlayerMatchesOverviewRequestMiddlewareWrapper", () {
+    group(".call()", () {
+      test(
+        "given GetPlayerMatchesOverviewRequestValidator instance"
+        "when request is passed to the middleware"
+        "then should call the provided GetPlayerMatchesOverviewRequestValidator.validate()",
+        () async {
+          // setup
+          final request = Request("post", Uri.parse("https://example.com/"));
+
+          // given
+          final validator = _MockGetPlayerMatchesOverviewRequestValidator();
+          when(() => validator.validate(
+                validatedRequestHandler: any(named: "validatedRequestHandler"),
+              )).thenReturn((request) {
+            return (Request request) async {
+              return Response.ok("ok");
+            }(request);
+          });
+
+          // get middlewareWrapper
+          final middlewareWrapper =
+              GetPlayerMatchesOverviewRequestMiddlewareWrapper(
+            getPlayerMatchesOverviewRequestValidator: validator,
+          );
+
+          // get middleware from the wrapper
+          final middleware = middlewareWrapper.call();
+
+          // get middleware request handler
+          final setupRequestHandler = middleware((request) async {
+            return Response.ok(
+                "Returning from the router-specific handler - request propagates to the request handler.");
+          });
+
+          // when
+          // send request to the middleware request handler
+          await setupRequestHandler(request);
+
+          // then
+          // verify that request handler was called
+          verify(() => validator.validate(
+                validatedRequestHandler: any(named: "validatedRequestHandler"),
+              )).called(1);
+
+          // cleanup
+        },
+      );
+    });
+  });
+}
+
+class _MockGetPlayerMatchesOverviewRequestValidator extends Mock
+    implements GetPlayerMatchesOverviewRequestValidator {}
