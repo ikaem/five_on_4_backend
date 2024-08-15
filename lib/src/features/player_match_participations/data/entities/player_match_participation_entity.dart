f0fcc493d201a99ebd20abf28de0925ffc507543
Karlo M <karlo.marinovic@decode.agency>
2024-08-15 07:38:26 +0200

    create player match participation table

---
+++ lib/src/features/player_match_participations/data/entities/player_match_participation_entity.dart
@@ -0,0 +1,14 @@
+import 'package:drift/drift.dart';
+import 'package:five_on_4_backend/src/features/matches/data/entities/match/match_entity.dart';
+import 'package:five_on_4_backend/src/features/players/data/entities/player/player_entity.dart';
+
+class PlayerMatchParticipationEntity extends Table {
+  IntColumn get id => integer().autoIncrement()();
+
+  DateTimeColumn get createdAt => dateTime()();
+  DateTimeColumn get updatedAt => dateTime()();
+
+  // refs
+  IntColumn get playerId => integer().references(PlayerEntity, #id)();
+  IntColumn get matchId => integer().references(MatchEntity, #id)();
+}
