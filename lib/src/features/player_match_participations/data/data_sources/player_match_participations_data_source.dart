37daf8806a95a1ec99950002c7da6e0060714d59
Karlo M <karlo.marinovic@decode.agency>
2024-08-17 08:57:39 +0200

    initial storeparticipatiuon

--- lib/src/features/player_match_participations/data/data_sources/player_match_participations_data_source.dart
+++ lib/src/features/player_match_participations/data/data_sources/player_match_participations_data_source.dart
@@ -1,9 +1,15 @@
 import 'package:equatable/equatable.dart';
 
 abstract interface class PlayerMatchParticipationsDataSource {
+  // TODO deprecated
   Future<int> createParticipation({
     required CreatePlayerMatchParticipationValue value,
   });
+
+  // TODO create storeParticipation - it should be upsert, and it should use that unique combination to always write on the same one? right? not sure?
+  // Future<int> storeParticipation({
+  //   required StorePlayerMatchParticipationValue value,
+  // });
 }
 
 // TODO not needed yet
@@ -13,6 +19,15 @@ class PlayerMatchParticipationEntityValue extends Equatable {
   // TODO: implement props
   List<Object?> get props => throw UnimplementedError();
 }
+
+// TODO move to values
+// class StorePlayerMatchParticipationValue extends Equatable {
+//   final int playerId;
+//   final int matchId;
+//   // TODO this could possibly have a default value
+//   final int createdAt;
+//   final int updatedAt;
+// }
 
 class CreatePlayerMatchParticipationValue {
   CreatePlayerMatchParticipationValue({
